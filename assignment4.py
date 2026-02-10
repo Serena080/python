2c8e467162a7cedc54d61e3bac8543390b95cea1
Serena080 <enaneserena8@gmail.com>
2026-02-10 10:05:06 +0300

    added new files

---
+++ assignment4.py
@@ -0,0 +1,26 @@
+#Create a List of Colors Blue, Green, Red, Pink , Black- Using a for Loop, Loop through the
+# Colors
+
+colors = ("Blue", "Green", "Red", "Pink", "Black")
+
+print(colors)
+
+for color in colors:
+     print(color)
+
+
+
+print("=============================================")
+#Using a While Loop Print from 20 to 1 - - Lesson4Task2.py
+
+for number in range(20, 0, -1):
+     print(number)
+
+
+
+
+print("=============================================")
+#Using a For Loop Print from 2000 to 2024 - Lesson4Task1.py
+
+for year in range(2000, 2025):
+     print(year)
