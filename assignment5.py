eb9c07241dac8918a85322dbb56b38db4b3415a8
Serena080 <enaneserena8@gmail.com>
2026-02-11 10:58:26 +0300

    added new files

---
+++ assignment5.py
@@ -0,0 +1,48 @@
+#Qn 1: Function Without Parameters
+#Create a function that:
+#• Takes no parameters
+# • Uses arithmetic operators to calculate the area of a rectangle
+# • Prints the result
+
+
+# Qn 2: Function With Parameters
+# Create a function that:
+# • Accepts two numbers as parameters
+# • Returns their sum, difference, product, and division
+
+
+# Qn 3: Control Statement (if...elif...else)
+# Write a function that:
+# • Accepts a number (use input function)
+# • Checks whether the number is:
+# • Positive
+# • Negative
+# • Zero
+
+
+# Qn 4: Loop with Arithmetic
+# Write a function that:
+# • Accepts a number n
+# • Uses a for loop
+# • Calculates the sum of numbers from 1 to n
+
+
+
+# Qn 5: While Loop
+# Write a function that:
+# • Accepts a number (Use input() function)
+# • Uses a while loop
+# • Calculates the square of numbers from 1 up to that number
+
+number1 = int(input("Enter the number:"))
+while number1 >= 1:
+     exponent = number1 ** 2
+     print("The exponent of the number: ", exponent)
+     
+
+
+   
+
+
+     
+     
